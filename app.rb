3e42221816cf66eda8dd3964ee2a6c953903abd1
pdinc <pavan.nyama@gmail.com>
2020-02-18 05:26:47 +0000

    Basic Setup

--- app.rb
+++ app.rb
@@ -6,13 +6,26 @@ require "httparty"
 def view(template); erb template.to_sym; end
 before { puts "Parameters: #{params}" }                                     
 
+# Don't forget attribution to Dark Sky and newsapi on site!
+
 # enter your Dark Sky API key here
-ForecastIO.api_key = "YOUR-API-KEY"
+ForecastIO.api_key = "02dbcee29621252b05da7abab52d2a8e"
+
+#Enter NewsApi here
+news_apikey = "81c2dc01131a474c806f57e65cf445be"
 
 get "/" do
-  # show a view that asks for the location
+    view "ask_location"
 end
 
 get "/news" do
-  # do everything else
+    @location = params["LocationInputBox"]
+    @location = @location.capitalize
+
+    @geocoder_results = Geocoder.search(@location)
+    @lat_long = @geocoder_results.first.coordinates # => [lat, long] array
+    @forecast = ForecastIO.forecast(@lat_long[0],@lat_long[1]).to_hash
+    #@curr_temp = @forecast["currently"]["temperature"]
+    #@curr_status = @forecast["currently"]["summary"]
+    pp @forecast
 end
